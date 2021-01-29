ededf734c5da36a965469eacbd3e92a2d153bd77
Eber Freire <eberfreire@gmail.com>
2021-01-29 16:56:26 -0600

    Test number 6

---
+++ GitTest6/GitTest6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 10; x < 15; x++)
+            {
+                Console.WriteLine("Value of x: {0}", x);
+            }
+        }
+    }
+}
